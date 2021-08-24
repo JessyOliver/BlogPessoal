72ff63769fac045a75ef761496c7e91cb5ad25fe
JessyOliver <jessy22oliver@gmail.com>
2021-08-24 15:30:28 -0300

    Componentes de menu, footer, cadastrar e entrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { MenuComponent } from './menu/menu.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntrarComponent,
+    RodapeComponent,
+    MenuComponent,
+    CadastrarComponent
   ],
   imports: [
     BrowserModule,
