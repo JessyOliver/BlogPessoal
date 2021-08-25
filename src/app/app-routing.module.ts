fd298c6f2f828a6c8fddc61378aedde9eb4241bd
JessyOliver <jessy22oliver@gmail.com>
2021-08-25 16:18:07 -0300

    Atualizando os campos para ficar transparente e redirecionando as paginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
-import { NgModule } from '@angular/core';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+import {  NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
   //toda vez que o campo for vazio, redirecione para a tela logar
   {path:'', redirectTo:'entrar', pathMatch:'full'},
+
+  //testando menu rodape
+  {path:'menu', component:MenuComponent}, 
+  {path:'rodape', component:RodapeComponent},
 
   //redirecionando para uma pagina especifica
   {path:'entrar', component: EntrarComponent},
