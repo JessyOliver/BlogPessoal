99c6d2cc2b07482b4637748ae396fc21cc0a1e28
JessyOliver <jessy22oliver@gmail.com>
2021-08-25 09:58:04 -0300

    paginas entrando em cada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //toda vez que o campo for vazio, redirecione para a tela logar
+  {path:'', redirectTo:'entrar', pathMatch:'full'},
+
+  //redirecionando para uma pagina especifica
+  {path:'entrar', component: EntrarComponent},
+  {path:'cadastrar', component: CadastrarComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
